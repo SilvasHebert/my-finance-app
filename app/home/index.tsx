9e9f1bd8b50570aca99aad6769054fa3addd193f
hebert.silva <hebert.silva@cibus.app.br>
2023-04-08 17:41:58 -0300

    initial commit

---
+++ app/home/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Link href="/index">Go to Index</Link>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+});
---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import "expo-router/entry";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Link href="/profile">Go to Profile</Link>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+});
